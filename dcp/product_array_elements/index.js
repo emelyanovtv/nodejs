0395b25f4e4daf16ace6d7ae79bec4a8bd230af7
emelyanovtv <emelyanovtv@gmail.com>
2019-05-06 22:28:53 +0300

    refactor(dcp): make less actions for product array items

--- dcp/product_array_elements/index.js
+++ dcp/product_array_elements/index.js
@@ -6,13 +6,13 @@ function productArrItemsWithDivision(arr) {
     if (arr.length <= 1) return arr;
 
     const comp = arr.reduce((acc, item) => {
-        if (item > 0) acc*=item;
+        if (item > 0) acc *= item;
 
         return acc;
     }, 1);
 
     return arr.map((item) => {
-        return comp/item ;
+        return comp / item;
     });
 }
 
@@ -21,24 +21,25 @@ assertArrays(productArrItemsWithDivision([3, 2, 1]), [2, 3, 6]);
 
 function productArrItems(arr) {
     if (arr.length <= 1) return arr;
+    const arrLength = arr.length - 1;
 
-    const left = makeNewArr(arr);
-    const right = makeNewArr(arr.reverse());
+    let curr = 1;
 
-    return arr.map((item, index) => {
-        return left[index] * right[right.length - 1 - index];
-    });
-}
+    const res = []
 
-function makeNewArr(arr) {
-    const result = [1];
-
-    for (let i = 0; i < arr.length - 1; i++) {
-        const prev = result[result.length - 1];
-        result.push(prev * arr[i]);
+    for (let i = 0; i <= arrLength; i++) {
+        res[i] = curr;
+        curr *= arr[i];
     }
 
-    return result;
+    curr = 1;
+
+    for (let i = arrLength; i >= 0; i--) {
+        res[i] = res[i] * curr;
+        curr *= arr[i];
+    }
+
+    return res;
 }
 
 assertArrays(productArrItems([1, 2, 3, 4, 5]), [120, 60, 40, 30, 24]);
